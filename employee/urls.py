4d149636703be99708c9442a85fc1843ad8b08f7
anishpdm <anish.vilayil.s2gmail.com>
2021-08-25 10:22:36 +0530

    Update and delete

--- employee/urls.py
+++ employee/urls.py
@@ -7,11 +7,19 @@ urlpatterns = [
     path('addview/', views.add_employee_view, name='add_employee_view'),
     path('viewemployeescreen/', views.view_all_employees, name='view_all_employees'),
     path('updateemployeescreen/', views.update_view_employees, name='update_view_employees'),
+    path('searchview/', views.search_view_employees, name='search_view_employees'),
+    path('deleteview/', views.delete_view_employees, name='delete_view_employees'),
     
 
 #    API's 
     path('add/', views.employee_create, name='employee_create'),
+    path('search/', views.searchapi, name='searchapi'),
+    path('Update_Search_api/', views.Update_Search_api, name='Update_Search_api'),
+    path('Delete_Search_api/', views.Delete_Search_api, name='Delete_Search_api'),
+
     path('viewall/', views.employee_list, name='employee_list'),
     path('viewemployee/<fetchid>', views.employee_details, name='employee_details'),
+    path('viewemployee/deletebtn/', views.employee_details_deletebtn, name='employee_details_deletebtn'),
+    path('viewemployee/updatebtn/', views.employee_details_updatebtn, name='employee_details_updatebtn'),
   
 ]
--- employee/views.py
+++ employee/views.py
@@ -9,6 +9,108 @@ from rest_framework import status
 import requests
 
 
+
+
+@csrf_exempt
+def Delete_Search_api(request):
+    try:
+        getEmpCode = request.POST.get("empcode")
+        getEmployee = Employee.objects.filter(empcode=getEmpCode)
+        employee_serialiser = EmployeeSerializer(getEmployee, many=True)
+        return render(request,"delete.html",{"data":employee_serialiser.data})
+        # return JsonResponse(employee_serialiser.data,safe=False,status=status.HTTP_200_OK)
+
+    except Employee.DoesNotExist:
+        return HttpResponse("Invalid Emp Code", status=status.HTTP_404_NOT_FOUND)
+        
+    except:
+        return HttpResponse("Something went wrong")    
+
+
+@csrf_exempt
+def employee_details_deletebtn(request):
+
+
+    newId = request.POST.get("newId")
+   
+    setUrl="http://localhost:8000/employee/viewemployee/"+newId
+    requests.delete(setUrl)
+    return HttpResponse("Deleted")
+
+
+@csrf_exempt
+def Update_Search_api(request):
+    try:
+        getEmpCode = request.POST.get("empcode")
+        getEmployee = Employee.objects.filter(empcode=getEmpCode)
+        employee_serialiser = EmployeeSerializer(getEmployee, many=True)
+        return render(request,"update.html",{"data":employee_serialiser.data})
+        # return JsonResponse(employee_serialiser.data,safe=False,status=status.HTTP_200_OK)
+
+    except Employee.DoesNotExist:
+        return HttpResponse("Invalid Emp Code", status=status.HTTP_404_NOT_FOUND)
+        
+    except:
+        return HttpResponse("Something went wrong")    
+
+
+@csrf_exempt
+def employee_details_updatebtn(request):
+
+
+    getName = request.POST.get("newname")
+    newId = request.POST.get("newId")
+    newempsalary = request.POST.get("newempsalary")
+    newempcode = request.POST.get("newempcode")
+    getEmployeeDesignation = request.POST.get("newempdesig")
+
+    mydata = {'name': getName, 'empdesig': getEmployeeDesignation,'empcode': newempcode,'empsalary': newempsalary}
+    jsondata=json.dumps(mydata)
+    print(jsondata)
+   
+    setUrl="http://localhost:8000/employee/viewemployee/"+newId
+    requests.put(setUrl, data=jsondata)
+    return HttpResponse("Updated")
+
+@csrf_exempt
+def employee_details_delete(request):
+
+
+    try:
+
+        getEmpCode = int(request.POST.get("empid"))
+        print(getEmpCode)
+        employees = Employee.objects.get(id=getEmpCode)
+        if (request.method == "POST"):
+            employees.delete()
+            return HttpResponse("Deleted", status=status.HTTP_204_NO_CONTENT)
+    except:
+        return HttpResponse("Invalid Emp Code", status=status.HTTP_404_NOT_FOUND)
+
+
+
+def search_view_employees(request):
+    return render(request, 'search.html')
+
+
+@csrf_exempt
+def searchapi(request):
+    try:
+        getEmpCode = request.POST.get("empcode")
+        getEmployee = Employee.objects.filter(empcode=getEmpCode)
+        employee_serialiser = EmployeeSerializer(getEmployee, many=True)
+        return render(request,"search.html",{"data":employee_serialiser.data})
+        # return JsonResponse(employee_serialiser.data,safe=False,status=status.HTTP_200_OK)
+
+    except Employee.DoesNotExist:
+        return HttpResponse("Invalid Emp Code", status=status.HTTP_404_NOT_FOUND)
+        
+    except:
+        return HttpResponse("Something went wrong")    
+
+def delete_view_employees(request):
+    return render(request, 'delete.html')
+    
 def update_view_employees(request):
     return render(request, 'update.html')
     
@@ -35,13 +137,22 @@ def employee_details(request, fetchid):
             return HttpResponse("Deleted",status=status.HTTP_204_NO_CONTENT)
 
         if (request.method == "PUT"):
+            # print(request.PUT)
+            # getName = request.PUT.get("name")
+           
+            # getEmployeeDesignation = request.PUT.get("empdesig")
+        
+
+            # mydata = {'name': getName,'empdesig': "fg",'empcode': '1001','empsalary': 0}
             mydata=JSONParser().parse(request)
+            print(mydata)
+            # mydata=JSONParser().parse(request)
             employee_serialize = EmployeeSerializer(employees,data=mydata)
             if (employee_serialize.is_valid()):
                 employee_serialize.save() #Update to Db
                 return JsonResponse(employee_serialize.data, status=status.HTTP_200_OK)
             else:
-                return JsonResponse(employee_serializer.errors,status=status.HTTP_400_BAD_REQUEST)    
+                return JsonResponse(employee_serialize.errors,status=status.HTTP_400_BAD_REQUEST)    
             
 
     except Employee.DoesNotExist: 
